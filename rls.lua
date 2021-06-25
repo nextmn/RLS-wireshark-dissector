6c6a3f00a54baa45c5751e746286a8f597dac491
Louis Royer <55180044+louisroyer@users.noreply.github.com>
2021-06-25 10:51:43 +0200

    Remove useless code

--- rls.lua
+++ rls.lua
@@ -7,60 +7,57 @@
 --
 --]]
 
--- This file must only be loaded once, but each version requires it.
---if package.loaded['rls'] == nil then
---	package.loaded['rls'] = true
-	local pluginVersion = "1.1.1"
-	-- Update the following when adding new versions
-	local latestVersion = 0x0302
-	local oldestVersion = 0x0301
+local pluginVersion = "1.1.1"
+-- Update the following when adding new versions
+local latestVersion = 0x0302
+local oldestVersion = 0x0301
 
-	local rlsProtocol = Proto("RLS", "UERANSIM Radio Link Simulation (RLS) Protocol")
-	set_plugin_info({
-		version = pluginVersion,
-		author = "Louis Royer",
-		repository = "https://github.com/louisroyer/RLS-wireshark-dissector",
-		description = "Dissector for Radio Link Simulation Protocol"
-	})
+local rlsProtocol = Proto("RLS", "UERANSIM Radio Link Simulation (RLS) Protocol")
+set_plugin_info({
+	version = pluginVersion,
+	author = "Louis Royer",
+	repository = "https://github.com/louisroyer/RLS-wireshark-dissector",
+	description = "Dissector for Radio Link Simulation Protocol"
+})
 
-	-- Create a DissectorTable to register dissector for each version of RLS
-	DissectorTable.new("rls", "RLS version", ftypes.UINT32, base.HEX, rlsProtocol)
+-- Create a DissectorTable to register dissector for each version of RLS
+DissectorTable.new("rls", "RLS version", ftypes.UINT32, base.HEX, rlsProtocol)
 
-	-- Preferences
-	rlsProtocol.prefs.udp_port = Pref.uint("RLS UDP port", 4997, "UDP port for RLS")
+-- Preferences
+rlsProtocol.prefs.udp_port = Pref.uint("RLS UDP port", 4997, "UDP port for RLS")
 
-	-- Add version field
-	local fields = rlsProtocol.fields
-	fields.Version = ProtoField.string("rls.version", "Version")
+-- Add version field
+local fields = rlsProtocol.fields
+fields.Version = ProtoField.string("rls.version", "Version")
 
-	function rlsProtocol.dissector(buffer, pinfo, tree)
-		-- Generic check
-		if buffer:len() == 0 then return end
-		if buffer(0, 1):uint() ~= 0x03 then return end
+function rlsProtocol.dissector(buffer, pinfo, tree)
+	-- Generic check
+	if buffer:len() == 0 then return end
+	if buffer(0, 1):uint() ~= 0x03 then return end
 
-		pinfo.cols.protocol = rlsProtocol.name
+	pinfo.cols.protocol = rlsProtocol.name
 
-		local version = buffer(1,2):uint()
-		local subprotocol = DissectorTable.get("rls"):get_dissector(version)
-		if subprotocol == nil then
-			if version > latestVersion then
-				-- fallback to latest version
-				version = latestVersion
-			elseif version < oldestVersion then
-				-- fallback to oldest version
-				version = oldestVersion
-			end
-			subprotocol = DissectorTable.get("rls"):get_dissector(version)
-			if subprotocol == nil then
-				local versionNumber = buffer(1, 1):uint() .. "." .. buffer(2, 1):uint() .. "." .. buffer(3, 1):uint()
-				local subtree = tree:add(rlsProtocol, buffer(), "UERANSIM Radio Link Simulation (RLS) protocol")
-				pinfo.cols.info = "Unsupported version - Cannot decode"
-				subtree:add(fields.Version, buffer(1, 3), versionNumber)
-				return 4
-			end
+	local version = buffer(1,2):uint()
+	local subprotocol = DissectorTable.get("rls"):get_dissector(version)
+	if subprotocol == nil then
+		if version > latestVersion then
+			-- fallback to latest version
+			version = latestVersion
+		elseif version < oldestVersion then
+			-- fallback to oldest version
+			version = oldestVersion
 		end
-		subprotocol:call(buffer():tvb(), pinfo, tree)
+		subprotocol = DissectorTable.get("rls"):get_dissector(version)
+		if subprotocol == nil then
+			local versionNumber = buffer(1, 1):uint() .. "." .. buffer(2, 1):uint() .. "." .. buffer(3, 1):uint()
+			local subtree = tree:add(rlsProtocol, buffer(), "UERANSIM Radio Link Simulation (RLS) protocol")
+			pinfo.cols.info = "Unsupported version - Cannot decode"
+			subtree:add(fields.Version, buffer(1, 3), versionNumber)
+			return 4
+		end
 	end
+	subprotocol:call(buffer():tvb(), pinfo, tree)
+end
 
 function rlsProtocol.init()
 	-- Export protocol
@@ -68,4 +65,3 @@ function rlsProtocol.init()
 	udp_port:add(rlsProtocol.prefs.udp_port, rlsProtocol)
 	udp_port:add_for_decode_as(rlsProtocol)
 end
---end
